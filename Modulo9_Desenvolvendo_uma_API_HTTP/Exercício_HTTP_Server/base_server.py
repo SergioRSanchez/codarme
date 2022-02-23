cb2ecdfbd4d7e4eead593cd2b4d448901307c6b1
Sergio Sanchez <usuario1@MacBook-Pro-de-usuario.local>
2022-02-23 14:40:44 -0300

    modulo 9 - Desenvolvendo uma API HTTP

--- Modulo9_Desenvolvendo_uma_API_HTTP/Exercício_HTTP_Server/base_server.py
+++ Modulo9_Desenvolvendo_uma_API_HTTP/Exercício_HTTP_Server/base_server.py
@@ -1,6 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import hashlib
-import json
 
 
 class Usuario:
@@ -20,27 +19,14 @@ class Usuario:
         print(f"Senha do usuário: {self.senha}")
         print("------------------------------------------")
 
-    def to_json(self):
-        return json.dumps({
-            "id": self.id,
-            "nome": self.nome,
-            "email": self.email,
-            "senha": self.senha
-        })
-
 
 def hash_string(texto):
-    """
-    Recebe um texto como string e retorna a representação hash desse texto.
-    Exemplo:
-        hash_string("123") -> "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07"
-    """
     return hashlib.sha256(texto.encode()).hexdigest()
 
 
-usuario1 = Usuario("Sergio", "sergio@exemplo.com", "1234567")
-usuario2 = Usuario("Carol", "carol@exemplo.com", "7654321")
-usuario3 = Usuario("Julia", "julia@exemplo.com", "283791731")
+usuario1 = Usuario("Alice", "alice@codar.me", "123456789")
+usuario2 = Usuario("Luíza", "luiza@codar.me", "987654321")
+usuario3 = Usuario("Natalia", "natalia@exemplo.com", "as76daf7a6sd8as")
 usuarios = [usuario1, usuario2, usuario3]
 
 
@@ -95,6 +81,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
             data = f"""
             <html>
               <head>{stylesheet}</head>
+              <h1>Usuários</h1>
               <table>
                 <tr>
                   <th>Id</th>
@@ -107,10 +94,6 @@ class SimpleHandler(BaseHTTPRequestHandler):
             </html>
             """.encode()
             self.wfile.write(data)
-            # Enviar o status code da resposta: self.send_reponse(status_code)
-            # Enviar cabeçalhos: self.send_header(nome, valor)
-            # Finalizar cabeçalhos: self.end_headers()
-            # Escrever dados para o "socket" (wfile): self.wfile.write(data)
             print("Implementar!")
 
 
