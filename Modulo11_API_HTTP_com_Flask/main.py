2dba24d31571fc0c495d211fec0f61d71b124d25
Sergio Sanchez <usuario1@MacBook-Pro-de-usuario.local>
2022-03-02 20:37:16 -0300

    modulo 11 - API HTTP com Flask

--- Modulo11_API_HTTP_com_Flask/main.py
+++ Modulo11_API_HTTP_com_Flask/main.py
@@ -2,6 +2,7 @@
     source venv/bin/activate,
     FLASK_APP=NomeDoArquivo.py FLASK_ENV=development flask run """
 
+from crypt import methods
 from flask import Flask, jsonify, abort, make_response, request, json
 from evento import Evento
 from evento_online import EventoOnline
@@ -65,19 +66,71 @@ informação que está vindo em um certo e formato, lendo ela e transformando em
 outra.
 """
 
+#  "Pegar" evento
+def get_evento_or_404(id):
+    for ev in eventos:
+        if ev.id == id:
+            return ev
+    abort(404, "Evento não encontrado.")
+
+#  Lidar com erro 400
 @app.errorhandler(400) 
 def nao_encontrado(erro):
     return (jsonify(erro=str(erro)), 400)
 
+#  Lidar com erro 404
 @app.errorhandler(404) 
 def nao_encontrado(erro):
     return (jsonify(erro=str(erro)), 404)
 
+#  Detalhar evento
 @app.route("/api/eventos/<int:id>/")
-def detalhar_evento(id):
-    for ev in eventos:
-        if ev.id == id:
-            return jsonify(ev.__dict__)
-    abort(404, "Evento não encontrado.")
-    #data = {"erro": f"Não encontrei evento com id: {id}"}
-    #return make_response(jsonify(data), 404)  Assim ele retorna um json
+def detalhar_evento(id):  #  view detalhar
+    ev = get_evento_or_404(id)
+    return jsonify(ev.__dict__)
+
+#  Deletar evento
+@app.route("/api/eventos/<int:id>/", methods=["DELETE"])
+def deletar_evento(id):  #  view deletar
+    ev = get_evento_or_404(id)
+    eventos.remove(ev)
+    return jsonify(id=id)
+
+#  Editar evento (usamos PUT quando for editar tudo do evento)
+@app.route("/api/eventos/<int:id>/", methods=["PUT"])
+def editar_evento(id):  #  view editar
+    #  Parsing
+    data = request.get_json()
+    nome = data.get("nome")
+    local = data.get("local")
+    #  Validar evento
+    if not nome:
+        abort(400, "'nome' precisa ser informado!")
+    if not local:
+        abort(400, "'local' precisa ser informado!")
+    
+    ev = get_evento_or_404(id)
+    ev.nome = nome
+    ev.local = local
+
+    return jsonify(ev.__dict__)
+
+#  Editar evento (usamos PATCH quando for editar alguma parte do evento)
+@app.route("/api/eventos/<int:id>/", methods=["PATCH"])
+def editar_evento_parcial(id):  #  view editar
+    #  Parsing
+    data = request.get_json()
+    ev = get_evento_or_404(id)
+    if "nome" in data.keys():
+        #  Pra editar o nome
+        nome = data.get("nome")
+        if not nome:
+            abort(400, "'nome' precisa ser informado!")
+        ev.nome = nome
+    
+    if "local" in data.keys():
+        local = data.get("local")
+        if not local:
+            abort(400, "'local' precisa ser informado!")
+        ev.local = local
+    return jsonify(ev.__dict__)
