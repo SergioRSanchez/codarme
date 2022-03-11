3c855d000e9db610dfc2dff204078d0f9c233ebc
SergioRSanchez <ssanchezfilho@gmail.com>
2022-03-11 10:46:22 -0300

    Modulo 12 - Django

--- Modulo12_Django/agenda/models.py
+++ Modulo12_Django/agenda/models.py
@@ -3,10 +3,19 @@ from django.db import models
 
 # Create your models here.9
 class Categoria(models.Model):
-    nome = models.CharField(max_length=256, unique=True)  #  CharField é texto
+    nome = models.CharField(max_length=256, blank=False,unique=True)  #  CharField é texto
 
     def __str__(self):
         return f"{self.nome} <{self.id}>"
+    
+    @classmethod
+    def cria_categoria(cls,nome):
+        if not nome:
+            raise ValueError("Categoria precisa de um nome.")
+        if nome:
+            categoria = Categoria(nome=nome)
+        categoria.save()
+        return categoria
 
 class Evento(models.Model):
     nome = models.CharField(max_length=256)
@@ -20,7 +29,16 @@ class Evento(models.Model):
         return f"{self.nome} <{self.id}>"
 
     @classmethod
-    def cria_evento(cls, nome, local, link, data, participantes):
-        evento = cls(nome, local="Rio Preto", link=f"https://tamarcado.com/eventos?id={cls.id}", data=data, participantes=participantes)
-        evento = Evento(nome, local, link, data, participantes)
+    def cria_evento(cls, nome, categoria, local=None, link=None, data=None):
+        if not nome:
+            raise ValueError("Evento precisa de um nome.")
+        if not categoria:
+            raise ValueError("Evento precisa de uma categoria.")
+        if local and link:
+            raise ValueError("Evento não pode possuir local e link.")
+        if local:
+            evento = Evento(nome=nome, categoria=categoria, local=local, data=data)
+        if link:
+            evento = Evento(nome=nome, categoria=categoria, link=link, data=data)
+        evento.save()
         return evento
