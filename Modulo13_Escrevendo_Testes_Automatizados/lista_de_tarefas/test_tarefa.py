906928e4aa070b914e82cea1dcf971042b18df78
SergioRSanchez <ssanchezfilho@gmail.com>
2022-03-15 22:37:54 -0300

    Modulo 13 - Escrevendo Testes Automatizados

---
+++ Modulo13_Escrevendo_Testes_Automatizados/lista_de_tarefas/test_tarefa.py
@@ -0,0 +1,22 @@
+import unittest
+from tarefa import Tarefa
+
+
+class TestConcluir(unittest.TestCase):
+    def test_concluir_tarefa_altera_concluida_para_true(self):
+        tarefa =  Tarefa("Estudar Python")
+        tarefa.concluir()
+        self.assertEqual(tarefa.concluida, True)
+    
+    def test_concluir_tarefa_concluida_mantem_concluida_como_true(self):
+        tarefa =  Tarefa("Estudar Python")
+        tarefa.concluir()
+        self.assertEqual(tarefa.concluida, True)
+        tarefa.concluir()
+        self.assertEqual(tarefa.concluida, True)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
