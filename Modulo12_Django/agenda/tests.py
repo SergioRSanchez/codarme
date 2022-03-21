746e26addf9cae1dbe86006c91632b4736a9146d
SergioRSanchez <ssanchezfilho@gmail.com>
2022-03-21 12:06:18 -0300

    Modulo 12 - Django

--- Modulo12_Django/agenda/tests.py
+++ Modulo12_Django/agenda/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from datetime import date
 
 # Create your tests here.
+
+from agenda.models import Evento, Categoria
+
+
+class TestPaginaInicial(TestCase):
+
+    def test_lista_eventos(self):
+        client = Client()
+        response = client.get("/")
+        #  print(response.content)
+        #  self.assertContains(response, "<th>Nome</th>")
+        self.assertTemplateUsed(response, "agenda/listar_eventos.html")
+
+
+class TestListagemDeEventos(TestCase):
+    
+    def test_evento_com_data_de_hoje_e_exibido(self):
+        categoria = Categoria()
+        categoria.nome = "Backend"
+        categoria.save()
+
+        evento = Evento()
+        evento.nome = "Aula de Python"
+        evento.categoria = categoria
+        evento.local = "Rio de Janeiro"
+        evento.data = date.today()
+        evento.save()
+
+        client = Client()
+        response = client.get("/")
+
+        self.assertContains(response, "Aula de Python")
+        self.assertEqual(response.context["eventos"][0], evento)
+        self.assertEqual(list(response.context["eventos"]), [evento])
+    
+
+    def test_evento_sem_data_sao_exibidos(self):
+        ...
--- Modulo12_Django/agenda/urls.py
+++ Modulo12_Django/agenda/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from agenda.views import exibir_evento, listar_eventos, participar_evento, exibir_categoria, listar_categorias
 
 urlpatterns = [
+    path("", listar_eventos, name="listar_eventos_raiz"),
     path("eventos/", listar_eventos, name="listar_eventos"),
     path("eventos/<int:id>/", exibir_evento, name="exibir_evento"),
     path("participar/", participar_evento, name="participar_evento"),
