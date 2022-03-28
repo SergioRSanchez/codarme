472f0e20aa92b31eaf92f3053f6167abed013797
SergioRSanchez <ssanchezfilho@gmail.com>
2022-03-28 20:29:31 -0300

    Modulo 14 - Django REST Framework

--- Modulo14_DRF_API/agenda/views.py
+++ Modulo14_DRF_API/agenda/views.py
@@ -3,9 +3,16 @@ from django.http import JsonResponse
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
+
+
 # Create your views here.
 
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.data)
+
+def agendamento_list(request):
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
+    return JsonResponse(serializer.data, safe=False)
