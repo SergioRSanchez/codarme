7ae7c064735bec38460ba0622d9b898cb0b79b27
SergioRSanchez <ssanchezfilho@gmail.com>
2022-03-29 20:23:13 -0300

    Modulo 14 - Django REST Framework

--- Modulo14_DRF_API/agenda/views.py
+++ Modulo14_DRF_API/agenda/views.py
@@ -9,7 +9,7 @@ from agenda.serializers import AgendamentoSerializer
 
 # Create your views here.
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "PUT"])
 def agendamento_detail(request, id):
     if request.method == "GET":
         #  Buscar instância de Agendamento
@@ -18,6 +18,19 @@ def agendamento_detail(request, id):
         serializer = AgendamentoSerializer(obj)
         #  Retornar JsonResponse com os dados do serializer
         return JsonResponse(serializer.data, status=200)
+    if request.method == "PUT":
+        obj = get_object_or_404(Agendamento, id=id)
+        serializer = AgendamentoSerializer(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            obj.data_horario = validated_data.get("data_horario", obj.data_horario)  # caso o novo valor passado dessa chave seja nulo, ele usa o valor antigo
+            obj.nome_cliente = validated_data.get("nome_cliente", obj.nome_cliente)
+            obj.email_cliente = validated_data.get("email_cliente", obj.email_cliente)
+            obj.telefone_cliente = validated_data.get("telefone_cliente", obj.telefone_cliente)
+            obj.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
 
 
 
@@ -42,3 +55,6 @@ def agendamento_list(request):
             return JsonResponse(serializer.data, status=201)
         # TODO: retornar JsonResponse com os erros do serializer e status 400
         return JsonResponse(serializer.errors, status=400)
+
+
+
