51443cb68d6c2dddb0a7a14eac2440912795dc08
SergioRSanchez <ssanchezfilho@gmail.com>
2022-03-31 13:10:58 -0300

    Modulo 14 - Django REST Framework

--- Modulo14_DRF_API/agenda/views.py
+++ Modulo14_DRF_API/agenda/views.py
@@ -11,15 +11,14 @@ from agenda.serializers import AgendamentoSerializer
 
 @api_view(http_method_names=["GET", "PUT", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
     if request.method == "GET":
         #  Buscar instância de Agendamento
-        obj = get_object_or_404(Agendamento, id=id)
         #  Instanciar serializer passando a instância de Agendamento
         serializer = AgendamentoSerializer(obj)
         #  Retornar JsonResponse com os dados do serializer
         return JsonResponse(serializer.data, status=200)
     if request.method == "PUT":  #  Esse método não se faz necessário, visto que temos o método Patch listado abaixo, porém deixei para posteriores consultas se necessárias
-        obj = get_object_or_404(Agendamento, id=id)
         serializer = AgendamentoSerializer(data=request.data)
         if serializer.is_valid():
             validated_data = serializer.validated_data
@@ -31,14 +30,13 @@ def agendamento_detail(request, id):
             return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
     if request.method == "PATCH":
-        obj = get_object_or_404(Agendamento, id=id)
-        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)  #  O partial é para que ,nesse caso, o requirimento das chaves no serializer não sejam todas obrigatório, fazendo com que eu não precise passar todas as chaves
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)  #  O partial é para que ,nesse caso, o requirimento das chaves no serializer não sejam todas obrigatório, fazendo com que eu não precise passar todas as chaves.
+        #  Se a gente passar a instância, representada por 'obj', a gene vai atualizar aquele objeto
         if serializer.is_valid():
-            serializer.save()
+            serializer.save()  # A FrameWork do Rest tem vários métodos e ordem de chamadas dos métodos, e nesse caso ele está chamando o método update no nosso Serializer em algum momentos
             return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
     if request.method == "DELETE":
-        obj = get_object_or_404(Agendamento, id=id)
         #  obj.delete()  # Ao invés de deletar e perder aquele registro, quero deixar salvo que foi cancelado, podendo dessa maneira exibir uma lista de objetos cancelados
         obj.horario_cancelado = True  # Para isso tenho que criar um atributo "cancelado" em models.py, e fazer a migração. Quando criamos o objeto, esse atributo está como 'False', e quando cancelamos, ele deve virar 'True'
         obj.save()
@@ -57,10 +55,10 @@ def agendamento_list(request):
     if request.method == "POST":
         data = request.data  #  {"nome_cliente": "Gabriel"...} -> Essa facilidade vem da api_view
         # Criar serializer a partir de `data`
-        serializer = AgendamentoSerializer(data=data)
+        serializer = AgendamentoSerializer(data=data)  # Passando somente o 'data' a gente cria o objeto
         if serializer.is_valid():  #  Faz toda aquela validação que fazíamos manualmente
             validated_data = serializer.validated_data  #  Quando o serializer.is_valid() está certo, automaticamente o serializer.validated_data é populado
-            serializer.save()
+            serializer.save()  # A FrameWork do Rest tem vários métodos e ordem de chamadas dos métodos, e nesse caso ele está chamando o método create no nosso Serializer em algum momentos
             return JsonResponse(serializer.data, status=201)
         # TODO: retornar JsonResponse com os erros do serializer e status 400
         return JsonResponse(serializer.errors, status=400)
