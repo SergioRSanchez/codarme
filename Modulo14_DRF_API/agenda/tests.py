7fa73c460cd466751df2fc3f4e06e676655814e5
SergioRSanchez <ssanchezfilho@gmail.com>
2022-04-04 15:21:54 -0300

    Modulo 14 - Django REST Framework

--- Modulo14_DRF_API/agenda/tests.py
+++ Modulo14_DRF_API/agenda/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+import json
+from agenda.models import Agendamento
+
 
 # Create your tests here.
+
+class TestListagemAgendamento(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content) #  json.loads(response.content) -> Converte o conteúdo da resposta em um dicionário
+        self.assertEqual(data, [])
+
+    def test_listagem_com_um_agendamento(self):
+        Agendamento.objects.create(data_horario="2019-01-01T00:00:00Z", nome_cliente="João", email_cliente="joao@email.com", telefone_cliente="(11) 99999-9999")
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        serializer = {
+            "id": 1,
+            "data_horario": "2019-01-01T00:00:00Z",
+            "nome_cliente": "João",
+            "email_cliente": "joao@email.com",
+            "telefone_cliente": "(11) 99999-9999"
+            }
+        self.assertEqual(data, [serializer])
+
+        
