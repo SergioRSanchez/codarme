db09b7d1912d4acc3920d3dd7472b74f832ede1e
SergioRSanchez <ssanchezfilho@gmail.com>
2022-04-07 09:04:47 -0300

    Modulo 14 - Django REST Framework

--- Modulo14_DRF_API/agenda/models.py
+++ Modulo14_DRF_API/agenda/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Agendamento(models.Model):
+    prestador = models.ForeignKey(
+        "auth.User", related_name="agendamentos", on_delete=models.CASCADE
+    )
+
     data_horario = models.DateTimeField()
     nome_cliente = models.CharField(max_length=255)
     email_cliente = models.EmailField()
